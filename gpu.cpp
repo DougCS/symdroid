706fce43285b805fc7b9b651bbd69be6b11abf61
BboyMUPO <sarkanyalex214@gmail.com>
2013-07-30 04:10:15 +0800

    Update gpu.cpp

--- gpu.cpp
+++ gpu.cpp
@@ -1,3 +1,12 @@
+#include "gpu.h"
+
 //Set frameskip to 0,1,2,3...
-int skipCount = 0
-int linesInternal = 0
+int skipCount = 0;
+int linesInterlace = 0;
+
+bool skipFrame = false;
+bool skipGPU = false;
+
+u8 BLEND_MODE;
+u8 TEXT_MODE;
+u8 Masking;
