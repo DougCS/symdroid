05e0629e7ecdb7fc1239b590d935eb1a6944a085
BboyMUPO <sarkanyalex214@gmail.com>
2013-07-31 14:08:53 +0800

    fixed 2 bugs

--- CPU.cpp
+++ CPU.cpp
@@ -1,6 +1,6 @@
 unsigned short opcode;
 #include "opcodes.h"
-//Symdroid will have 3gb memory
-unsigned char memory[3221225472];
-//The screen size will 480X800 pixels
-unsigned char gfx[480 * 800];
+//Symdroid will use 90mb ram
+unsigned char memory[7550008];
+//The screen size will 640X360 pixels
+unsigned char gfx[640 * 360];
