3f1a693273b77b82e2e87ee910c5968ff773e92e
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-09 19:32:45 +0800

    CPU overclock is usable

--- CPU.cpp
+++ CPU.cpp
@@ -4,3 +4,6 @@ unsigned short opcode;
 unsigned char memory[7550008];
 //The screen size will 640X360 pixels
 unsigned char gfx[640 * 360];
+//CPU overclock is not good to use because it will need more battery energy and batter cooling in use of it.It's better to be turned of but if you want you can turn it on ON //YOUR OWN RISK
+bool CPUoverclock = flase;
+int CPUoverclockrate = 667//in mhz
