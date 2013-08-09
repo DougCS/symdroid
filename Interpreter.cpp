d46bb648e3773ad0a25b1c999b8d3a19ea6b0136
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-09 23:40:43 +0800

    Update Interpreter.cpp

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -2,6 +2,27 @@
 #include "gpu.cpp"
 #include "SDpath.py"
 
-int openfile();
+unsigned char delay_timer;
+
+void openfile();
+{
     #include "filebrowser.py"
     return 0;
+}
+
+void symdroid::emulateCycle()
+{
+    if(delay_timer > 0)
+    --delay_timer;
+}
+
+void symdroid::initialize()
+{
+    opcode = 0;
+    I = 0;
+    sp = 0;
+}
+symdroid::emulateCycle();
+symdroid::initialize();
+
+return 0;
