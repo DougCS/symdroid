eaeb59a6d7c7b67ef4cd1a265aa9690d5fc93845
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-13 01:08:39 +0800

    interpreter rewritten in python for stability

---
+++ InterpreterB.py
@@ -0,0 +1,22 @@
+#Interpreter rewritten in python fo more stability
+import gpu
+import CPU
+import SDpath
+
+delay_timer
+
+def openfile():
+    import filebrowser
+
+def symdroid::emulatecycle():
+    if delay_timer > 0:
+        --delay_timer
+
+def symdroid::initialize():
+    opcode = 0
+    I = 0
+    sp = 0
+
+openfile()
+symdroid::emulatecycle()
+symdroid::initialize()
