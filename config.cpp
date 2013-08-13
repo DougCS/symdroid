90a9b73f1d04330dd4aa9c31f12b782a1b3d46d7
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-13 13:28:07 +0800

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -1,5 +1,54 @@
 #include "config.h"
 
-IniFile::Section *cpu = iniFile.GetOrCreateSection("CPU");
-cpu->Set("Jit", bJit);
-cpu->Set("FastMemory", bFastMemory);
+Config::Config() { }
+Config::~Config() { }
+
+void Config::Load(const char *iniFileName)
+{
+    iniFilename_ = iniFileName;
+    INFO_LOG(LOADER, "Loading config: %s", iniFileName);
+    bSaveSettings = true;
+
+    IniFile iniFile;
+    if (!iniFile.Load(iniFileName)) {
+    ERROR_LOG(LOADER, "Failed to read %s. Setting config to default.", iniFileName);
+    }
+
+    IniFile::Section *cpu = iniFile.GetOrCreateSection("CPU");
+    cpu->Get("Jit", &bJit, true);
+    cpu->Get("FastMemory", &bFastMemory, false);
+
+    graphics->Get("HardwareTransform", &bHardwareTransform, true);
+
+IniFile::Section *sound = iniFile.GetOrCreateSection("Sound");
+sound->Get("Enable", &bEnableSound, false);
+sound->Get("VolumeBGM", &iBGMVolume, 4);
+sound->Get("VolumeSFX", &iSFXVolume, 4);
+
+void Config::Save()
+{
+    if (iniFilename_.size() && g_Config.bSaveSettings) {
+        CleanRecent();
+        IniFile iniFile;
+        if (!iniFile.Load(iniFilename_.c_str())) {
+            ERROR_LOG(LOADER, "Error saving config - can't read ini %s", iniFilename_.c_str());
+        }
+
+    IniFile::Section *cpu = iniFile.GetOrCreateSection("CPU");
+    cpu->Set("Jit", bJit);
+    cpu->Set("FastMemory", bFastMemory);
+
+    IniFile::Section *sound = iniFile.GetOrCreateSection("Sound");
+    sound->Set("Enable", bEnableSound);
+    sound->Set("VolumeBGM", iBGMVolume);
+    sound->Set("VolumeSFX", iSFXVolume);
+
+    if (!iniFile.Save(iniFilename_.c_str())) {
+        ERROR_LOG(LOADER, "Error saving config - can't write ini %s", iniFilename_.c_str());
+        return;
+    }
+    INFO_LOG(LOADER, "Config saved: %s", iniFilename_.c_str());
+    } else {
+    INFO_LOG(LOADER, "Not saving config");
+    }
+}
