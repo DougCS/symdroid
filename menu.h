e274798ff9c0d6df3c739a2414b60c9b91766af2
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-17 01:39:05 +0800

    Update menu.h

--- menu.h
+++ menu.h
@@ -9,3 +9,48 @@ public:
 private:
     int frames_;
 };
+
+class SettingsScreen : public screen
+{
+public:
+    void update(InputState &input);
+    void render();
+}
+
+class AudioScreen : public Screen
+{
+public:
+    void update(InputState &input);
+    void render();
+};
+
+class GraphicsScreen : public Screen
+{
+public:
+    void update(InputState &input);
+void render();
+};
+
+class SystemScreen : public Screen
+{
+public: void update(InputState &input);
+void render();
+};
+
+class FileSelectScreen : public Screen
+{
+public: FileSelectScreen(const FileSelectScreenOptions &options);
+    void update(InputState &input);
+    void render();
+    virtual void onSelectFile() {}
+    virtual void onCancel() {}
+    void key(const KeyInput &key);
+
+private:
+    void updateListing();
+
+    FileSelectScreenOptions options_;
+    UIList list_;
+    std::string currentDirectory_;
+    std::vector<FileInfo> listing_;
+};
