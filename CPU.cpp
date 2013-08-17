4c20090874978e8a0aa58a2dd9ec5b0057e4de38
utvebs <vaibhya11@gmail.com>
2013-08-17 13:06:13 +0530

    Update CPU.cpp

--- CPU.cpp
+++ CPU.cpp
@@ -5,7 +5,7 @@ unsigned char memory[7550008];
 //The screen size will 640X360 pixels
 unsigned char gfx[640 * 360];
 //CPU overclock is not good to use because it will need more battery energy and batter cooling in use of it.It's better to be turned of but if you want you can turn it on ON YOUR OWN RISK
-bool CPUoverclock = flase;
+bool CPUoverclock = false;
 int CPUoverclockrate = 667//in mhz
 
 Bitu CPU_ArchitectureType = CPU_ARCHTYPE_MIXED;
