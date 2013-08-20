1bc2b8f61d69538bc612fa38b02e2d429990f122
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-20 14:28:28 +0800

    now i'm working on it.it now have psp build code just for the time i do it finally

---
+++ build-symbian.bat
@@ -0,0 +1,5 @@
+//First I know that not the // the comment line in batch.This is the build program that will make the .sis package.I'm working on it so don't open a new issue for it
+PATH=c:\symbiansdk\
+make clean
+make
+pause
