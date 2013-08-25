227d8c5b15c1efc6e5519e8be2374feb6eb80088
simon816 <simonbarnes1@googlemail.com>
2013-08-25 11:24:51 +0100

    Improves SDpath.py
    Moved loose functions to a class, added better testing

--- SDpath.py
+++ SDpath.py
@@ -1,24 +1,52 @@
 import os
 
-def write_symdroidData():
+class SymdroidData:
     CONFIG_DIR='e:/symdroid/SDdata'
-    CONFIG_FILE=os.path.join(CONFIG_DIR,'SDData.obb')
-    if not os.path.isdir(CONFIG_DIR):
-        os.makedirs(CONFIG_DIR)
-
-def read_SDdata():
-    CONFIG_FILE='e:/symdroid/SDdata/SDData.obb'
-    try:
-        f=open(CONFIG_FILE,'rt')
+    CONFIG_FILENAME='SDData.obb'
+    def __init__(self):
+        self.CONFIG_FILE=os.path.join(self.CONFIG_DIR,self.CONFIG_FILENAME)
+        if not os.path.isdir(self.CONFIG_DIR):
+            os.makedirs(self.CONFIG_DIR)
+    def write(self, data):
+        if type(data) is not dict:
+            print 'Data not in the right format'
+            return
         try:
-            content = f.read()
-            config=eval(content)
+            f=open(self.CONFIG_FILE,'wt')
+            f.write(str(data))
             f.close()
-        except:
-            print 'Can not read file'
-    except:
-        print 'Can not open file'
+        except IOError:
+            print 'Cannot write to file'
+    def read(self):
+        try:
+            f=open(self.CONFIG_FILE,'rt')
+            try:
+                content = f.read()
+                config=eval(content)
+                f.close()
+                return config
+            except:
+                print 'Cannot read file'
+        except IOError:
+            print 'Cannot open file'
 
-write_symdroidData()
 
-read_SDdata()
+if __name__=="__main__":
+    ### Testing ###
+    sd=SymdroidData()
+    print "Data path:", sd.CONFIG_FILE
+    data= {
+        'variable1' : 'value1',
+        'variable2' : 'value2'
+    }
+    print 'Attempting to write data:',data
+    sd.write(data)
+    print 'Reading data'
+    newdata=sd.read()
+    if newdata != data:
+        print 'Error, data missmatch'
+    else:
+        print 'Data success'
+        print newdata.get('variable1')
+        print newdata.get('variable2')
+    print 'Finished'
