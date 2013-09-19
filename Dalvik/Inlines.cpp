abafd98cb2a057ec37bd770b83f4ebeece49fa80
BboyMUPO <sarkanyalex214@gmail.com>
2013-09-19 18:38:31 +0200

    Create Inlines.cpp

---
+++ Dalvik/Inlines.cpp
@@ -0,0 +1,8 @@
+#define _DALVIK_GEN_INLINES
+#include "Dalvik.h"
+#include "analysis/CodeVerify.h"
+#include "analysis/RegisterMap.h"
+#include "mterp/c/header.cpp"
+
+#undef LOG_TAG
+#include "jdwp/JdwpPriv.h"
