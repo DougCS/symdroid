a169b81b4bf1e7d389722febfa910a32c1b64870
BboyMUPO <sarkanyalex214@gmail.com>
2013-10-03 16:04:02 +0200

    Create os.h

---
+++ Dalvik/os.h
@@ -0,0 +1,9 @@
+struct Thread;
+
+int os_raiseThreadPriority();
+
+void os_lowerThreadPriority(int oldThreadPriority);
+
+void os_changeThreadPriority(Thread* thread, int newPriority);
+
+int os_getThreadPriorityFromSystem();
