a0a91b60cc3bf298904fee6dac6b3eeff6cd740e
BboyMUPO <sarkanyalex214@gmail.com>
2013-10-03 16:02:50 +0200

    Create OptInvocation.h

---
+++ Dalvik/OptInvocation.h
@@ -0,0 +1,8 @@
+#ifndef LIBDEX_OPTINVOCATION_H_
+#define LIBDEX_OPTINVOCATION_H_
+
+char* dexOptGenerateCacheFileName(const char* fileName,
+    const char* subFileName);
+int dexOptCreateEmptyHeader(int fd);
+
+#endif
