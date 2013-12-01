a13f4094a7abacea0cd4d7bddc25b5b253df40cd
BboyMUPO <sarkanyalex214@gmail.com>
2013-12-01 18:36:31 +0100

    Update Interpreter.cpp

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -6,19 +6,28 @@
 
 class symdroid {
 	int a;
+	int overclock;
+	overclock = 0; //turn overclock off
 public:
 	static void openfile();
 	static void emulateCycle();
 	static void initialize();
+	static void overclock();
 };
 void symdroid::openfile() {
 	#include "Filemanager.cpp";
 }
 
 void symdroid::initialize() {
-	opcode = 0;
+        opcode = 0;
 	int sp = 0;
 	int pc = 0;
+}
+
+if overclock == 1; {
+	void phone::overclock() {
+	        #include "cpuoverclock"
+	}
 }
 
 int main() {
